8cc35b28e10fc005470e80c114f3bbc5742674dc
Gilles Lengy <gilles.lengy@artaban.fr>
2017-01-02 17:44:53 +0100

    Création de l'entité Animal avec son Repository basique

---
+++ src/VetoPlatformBundle/Entity/Animal.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace VetoPlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Animal
+ *
+ * @ORM\Table(name="animal")
+ * @ORM\Entity(repositoryClass="VetoPlatformBundle\Repository\AnimalRepository")
+ */
+class Animal
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255)
+     */
+    private $nom;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_naissance", type="datetime", nullable=true)
+     */
+    private $dateNaissance;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="commentaire", type="text", nullable=true)
+     */
+    private $commentaire;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date_maj", type="datetime")
+     */
+    private $dateMaj;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return Animal
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+
+    /**
+     * Set dateNaissance
+     *
+     * @param \DateTime $dateNaissance
+     *
+     * @return Animal
+     */
+    public function setDateNaissance($dateNaissance)
+    {
+        $this->dateNaissance = $dateNaissance;
+
+        return $this;
+    }
+
+    /**
+     * Get dateNaissance
+     *
+     * @return \DateTime
+     */
+    public function getDateNaissance()
+    {
+        return $this->dateNaissance;
+    }
+
+    /**
+     * Set commentaire
+     *
+     * @param string $commentaire
+     *
+     * @return Animal
+     */
+    public function setCommentaire($commentaire)
+    {
+        $this->commentaire = $commentaire;
+
+        return $this;
+    }
+
+    /**
+     * Get commentaire
+     *
+     * @return string
+     */
+    public function getCommentaire()
+    {
+        return $this->commentaire;
+    }
+
+    /**
+     * Set dateMaj
+     *
+     * @param \DateTime $dateMaj
+     *
+     * @return Animal
+     */
+    public function setDateMaj($dateMaj)
+    {
+        $this->dateMaj = $dateMaj;
+
+        return $this;
+    }
+
+    /**
+     * Get dateMaj
+     *
+     * @return \DateTime
+     */
+    public function getDateMaj()
+    {
+        return $this->dateMaj;
+    }
+}
+
