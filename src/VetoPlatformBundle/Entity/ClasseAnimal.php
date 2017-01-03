0b41d64494de143283dc57c0bc1e03a214491e8b
Gilles Lengy <gilles.lengy@artaban.fr>
2017-01-03 16:27:29 +0100

    Génération entité ClasseAnimal

---
+++ src/VetoPlatformBundle/Entity/ClasseAnimal.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace VetoPlatformBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ClasseAnimal
+ *
+ * @ORM\Table(name="classe_animal")
+ * @ORM\Entity(repositoryClass="VetoPlatformBundle\Repository\ClasseAnimalRepository")
+ */
+class ClasseAnimal
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255, unique=true)
+     */
+    private $nom;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set nom
+     *
+     * @param string $nom
+     *
+     * @return ClasseAnimal
+     */
+    public function setNom($nom)
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * Get nom
+     *
+     * @return string
+     */
+    public function getNom()
+    {
+        return $this->nom;
+    }
+}
+
